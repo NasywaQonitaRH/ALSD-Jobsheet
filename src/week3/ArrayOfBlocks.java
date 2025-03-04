55fbaaa4b0b6d3eb7bedc9cac337972dd4ddcf58
Nasywa <nasywaqonitarh1010@gmail.com>
2025-03-04 12:38:46 +0700

    update ArrayOfBlocks

--- src/week3/ArrayOfBlocks.java
+++ src/week3/ArrayOfBlocks.java
@@ -1,15 +1,14 @@
 package week3;
 
-
 public class ArrayOfBlocks {
     public static void main(String[] args) {
         Blocks[] blArray = new Blocks[3];
-        
-        blArray[0] = new Blocks(10, 10, 10);
-        blArray[1] = new Blocks(10, 10, 20);
-        blArray[2] = new Blocks(20, 15, 25);
-        
-        for (int i = 0; i < blArray.length; i++) {
+
+        blArray[0] = new Blocks(10, 12, 300);
+        blArray[1] = new Blocks(10, 12, 600);
+        blArray[2] = new Blocks(20, 25, 250);
+
+        for (int i = 0; i < 3; i++) {
             System.out.println("Volume blocks - " + i + " = " + blArray[i].countVolume());
         }
     }
--- src/week3/Blocks.java
+++ src/week3/Blocks.java
@@ -1,14 +1,14 @@
 package week3;
 
-class Blocks {
+public class Blocks {
     public int length, width, height;
-    
+
     public Blocks(int l, int w, int h) {
         length = l;
         width = w;
         height = h;
     }
-    
+
     public int countVolume() {
         return length * width * height;
     }
